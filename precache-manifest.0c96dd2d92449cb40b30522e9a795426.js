70fe1626c6c2bc5e2377169f4b10d44b68babbea
Shan Gardezi <shangardezi@gmail.com>
2020-08-20 23:02:12 +0100

    Updates

---
+++ precache-manifest.0c96dd2d92449cb40b30522e9a795426.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087c6bb2b19e0988f25a4d8f1807a555",
+    "url": "/cf-testing-achievement-4/index.html"
+  },
+  {
+    "revision": "fe3088b3abeb9727fb0c",
+    "url": "/cf-testing-achievement-4/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "47648c616e649b715f42",
+    "url": "/cf-testing-achievement-4/static/js/2.63d82722.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cf-testing-achievement-4/static/js/2.63d82722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe3088b3abeb9727fb0c",
+    "url": "/cf-testing-achievement-4/static/js/main.cb1af78a.chunk.js"
+  },
+  {
+    "revision": "04df3b5abaa793b34bf1",
+    "url": "/cf-testing-achievement-4/static/js/runtime-main.a05217dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cf-testing-achievement-4/static/media/logo.5d5d9eef.svg"
+  }
+]);
